2012abb96242dadeda18d65be72b223f2a54b377
Lucas Dias <sperandio39@gmail.com>
2024-09-28 17:32:28 -0300

    teste

--- test/teste.py
+++ test/teste.py
@@ -1,41 +1,44 @@
 from src.main import *
 from unittest.mock import patch
+import pytest
 
-def teste_root():
+@pytest.mark.asyncio
+async def teste_root():
     result = root()
-    yield result
+    result = await root( )
     assert result == {"message": "Hello World"}
 
-def teste_funcaoteste():
+@pytest.mark.asyncio
+async def teste_funcaoteste():
     with patch('random.randint', return_value = 12345):
-        result = funcaoteste()
-        yield result
+        result = await funcaoteste()
     assert result == {"teste": True, "num_aleatorio": 12345}
 
-def teste_create_estudante(estudante:Estudante):
+@pytest.mark.asyncio
+async def teste_create_estudante(estudante:Estudante):
     estudante_teste = Estudante(name="Fulano", curso="Curso Teste", ativo="false")
-    result = create_estudante(estudante_teste)
-    yield result
+    result = await create_estudante(estudante_teste)
     assert estudante_teste == result
 
-def teste_update_estudante_negativo(id_estudante: int):
-    result = update_estudante(-5)
-    yield result
+@pytest.mark.asyncio
+async def teste_update_estudante_negativo(id_estudante: int):
+    result = await update_estudante(-5)
     assert not result
 
-def teste_update_estudante_positivo(id_estudante: int):
-    result = update_estudante(10)
-    yield result
+@pytest.mark.asyncio
+async def teste_update_estudante_positivo(id_estudante: int):
+    result = await update_estudante(10)
     assert not result
 
-def teste_delete_estudante_negativo(id_estudante: int):
-    result = delete_estudante(-5)
-    yield result
+
+@pytest.mark.asyncio
+async def teste_delete_estudante_negativo(id_estudante: int):
+    result = await delete_estudante(-5)
     assert not result
 
-def teste_delete_estudante_positivo(id_estudante: int):
-    result = delete_estudante(5)
-    yield result
-    assert not result
 
+@pytest.mark.asyncio
+async def teste_delete_estudante_positivo(id_estudante: int):
+    result = await delete_estudante(5)
+    assert not result
 
