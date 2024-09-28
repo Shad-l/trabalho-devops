6f309114a72110241a6c23dc40a958f389c9cc18
Lucas Dias <sperandio39@gmail.com>
2024-09-28 17:37:26 -0300

    teste

--- test/teste.py
+++ test/teste.py
@@ -4,41 +4,37 @@ import pytest
 
 @pytest.mark.asyncio
 async def teste_root():
-    result = root()
-    result = await root( )
+    result = await root()
     assert result == {"message": "Hello World"}
 
 @pytest.mark.asyncio
 async def teste_funcaoteste():
-    with patch('random.randint', return_value = 12345):
+    with patch('random.randint', return_value=12345):
         result = await funcaoteste()
     assert result == {"teste": True, "num_aleatorio": 12345}
 
 @pytest.mark.asyncio
-async def teste_create_estudante(estudante:Estudante):
-    estudante_teste = Estudante(name="Fulano", curso="Curso Teste", ativo="false")
+async def teste_create_estudante():
+    estudante_teste = Estudante(name="Fulano", curso="Curso Teste", ativo=False)
     result = await create_estudante(estudante_teste)
-    assert estudante_teste == result
+    assert result == estudante_teste
 
 @pytest.mark.asyncio
-async def teste_update_estudante_negativo(id_estudante: int):
+async def teste_update_estudante_negativo():
     result = await update_estudante(-5)
-    assert not result
+    assert result == False
 
 @pytest.mark.asyncio
-async def teste_update_estudante_positivo(id_estudante: int):
+async def teste_update_estudante_positivo():
     result = await update_estudante(10)
-    assert not result
-
+    assert result == True
 
 @pytest.mark.asyncio
-async def teste_delete_estudante_negativo(id_estudante: int):
+async def teste_delete_estudante_negativo():
     result = await delete_estudante(-5)
-    assert not result
-
+    assert result == False
 
 @pytest.mark.asyncio
-async def teste_delete_estudante_positivo(id_estudante: int):
+async def teste_delete_estudante_positivo():
     result = await delete_estudante(5)
-    assert not result
-
+    assert result == True
